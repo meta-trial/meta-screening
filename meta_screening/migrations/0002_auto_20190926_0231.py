e6bad06f057c608aa15a464fea8a0d7c0c877d74
erikvw <ew2789@gmail.com>
2019-09-26 00:38:05 -0500

    black format

--- meta_screening/migrations/0002_auto_20190926_0231.py
+++ meta_screening/migrations/0002_auto_20190926_0231.py
@@ -7,59 +7,137 @@ import django_crypto_fields.fields.encrypted_char_field
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('meta_screening', '0001_initial'),
-    ]
+    dependencies = [("meta_screening", "0001_initial")]
 
     operations = [
         migrations.AlterField(
-            model_name='historicalscreeningpartone',
-            name='hospital_identifier',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, db_index=True, help_text=' (Encryption: RSA local)', max_length=71),
+            model_name="historicalscreeningpartone",
+            name="hospital_identifier",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                db_index=True,
+                help_text=" (Encryption: RSA local)",
+                max_length=71,
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalscreeningpartone',
-            name='initials',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text='Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)', max_length=71, validators=[django.core.validators.RegexValidator('[A-Z]{1,3}', 'Invalid format'), django.core.validators.MinLengthValidator(2), django.core.validators.MaxLengthValidator(3)]),
+            model_name="historicalscreeningpartone",
+            name="initials",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text="Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)",
+                max_length=71,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        "[A-Z]{1,3}", "Invalid format"
+                    ),
+                    django.core.validators.MinLengthValidator(2),
+                    django.core.validators.MaxLengthValidator(3),
+                ],
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalscreeningpartthree',
-            name='hospital_identifier',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, db_index=True, help_text=' (Encryption: RSA local)', max_length=71),
+            model_name="historicalscreeningpartthree",
+            name="hospital_identifier",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                db_index=True,
+                help_text=" (Encryption: RSA local)",
+                max_length=71,
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalscreeningpartthree',
-            name='initials',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text='Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)', max_length=71, validators=[django.core.validators.RegexValidator('[A-Z]{1,3}', 'Invalid format'), django.core.validators.MinLengthValidator(2), django.core.validators.MaxLengthValidator(3)]),
+            model_name="historicalscreeningpartthree",
+            name="initials",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text="Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)",
+                max_length=71,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        "[A-Z]{1,3}", "Invalid format"
+                    ),
+                    django.core.validators.MinLengthValidator(2),
+                    django.core.validators.MaxLengthValidator(3),
+                ],
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalscreeningparttwo',
-            name='hospital_identifier',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, db_index=True, help_text=' (Encryption: RSA local)', max_length=71),
+            model_name="historicalscreeningparttwo",
+            name="hospital_identifier",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                db_index=True,
+                help_text=" (Encryption: RSA local)",
+                max_length=71,
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalscreeningparttwo',
-            name='initials',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text='Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)', max_length=71, validators=[django.core.validators.RegexValidator('[A-Z]{1,3}', 'Invalid format'), django.core.validators.MinLengthValidator(2), django.core.validators.MaxLengthValidator(3)]),
+            model_name="historicalscreeningparttwo",
+            name="initials",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text="Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)",
+                max_length=71,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        "[A-Z]{1,3}", "Invalid format"
+                    ),
+                    django.core.validators.MinLengthValidator(2),
+                    django.core.validators.MaxLengthValidator(3),
+                ],
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalsubjectscreening',
-            name='hospital_identifier',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, db_index=True, help_text=' (Encryption: RSA local)', max_length=71),
+            model_name="historicalsubjectscreening",
+            name="hospital_identifier",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                db_index=True,
+                help_text=" (Encryption: RSA local)",
+                max_length=71,
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalsubjectscreening',
-            name='initials',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text='Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)', max_length=71, validators=[django.core.validators.RegexValidator('[A-Z]{1,3}', 'Invalid format'), django.core.validators.MinLengthValidator(2), django.core.validators.MaxLengthValidator(3)]),
+            model_name="historicalsubjectscreening",
+            name="initials",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text="Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)",
+                max_length=71,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        "[A-Z]{1,3}", "Invalid format"
+                    ),
+                    django.core.validators.MinLengthValidator(2),
+                    django.core.validators.MaxLengthValidator(3),
+                ],
+            ),
         ),
         migrations.AlterField(
-            model_name='subjectscreening',
-            name='hospital_identifier',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text=' (Encryption: RSA local)', max_length=71, unique=True),
+            model_name="subjectscreening",
+            name="hospital_identifier",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text=" (Encryption: RSA local)",
+                max_length=71,
+                unique=True,
+            ),
         ),
         migrations.AlterField(
-            model_name='subjectscreening',
-            name='initials',
-            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(blank=True, help_text='Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)', max_length=71, validators=[django.core.validators.RegexValidator('[A-Z]{1,3}', 'Invalid format'), django.core.validators.MinLengthValidator(2), django.core.validators.MaxLengthValidator(3)]),
+            model_name="subjectscreening",
+            name="initials",
+            field=django_crypto_fields.fields.encrypted_char_field.EncryptedCharField(
+                blank=True,
+                help_text="Use UPPERCASE letters only. May be 2 or 3 letters. (Encryption: RSA local)",
+                max_length=71,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        "[A-Z]{1,3}", "Invalid format"
+                    ),
+                    django.core.validators.MinLengthValidator(2),
+                    django.core.validators.MaxLengthValidator(3),
+                ],
+            ),
         ),
     ]
