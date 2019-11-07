a78226a6988f3cfbde9ae34b3e6be66643fcaedf
erikvw <ew2789@gmail.com>
2019-11-07 00:10:22 -0600

    prevent subject refusal save if not eligible/consented, fix url to subject dashboard

--- meta_screening/forms/subject_refusal_form.py
+++ meta_screening/forms/subject_refusal_form.py
@@ -3,30 +3,38 @@ from django.urls.base import reverse
 from django.utils.safestring import mark_safe
 from edc_dashboard.url_names import url_names
 from edc_form_validators import FormValidatorMixin
+from edc_screening.modelform_mixins import AlreadyConsentedFormMixin
 
 from ..form_validators import SubjectRefusalFormValidator
-from ..models import SubjectRefusal
+from ..models import SubjectRefusal, SubjectScreening
 
 
-class ConsentedFormMixin:
+class ScreeningFormMixin:
     def clean(self):
         cleaned_data = super().clean()
-        if self.instance.id:
-            url_name = url_names.get("screening_listboard_url")
-            url = reverse(
-                url_name,
-                kwargs={"screening_identifier": self.instance.screening_identifier},
+        screening_identifier = cleaned_data.get("screening_identifier")
+        if screening_identifier:
+            subject_screening = SubjectScreening.objects.get(
+                screening_identifier=screening_identifier
             )
-            msg = mark_safe(
-                "This form is not relevant. Subject has already consented. "
-                f'See subject <A href="{url}?q={self.instance.screening_identifier}">'
-                f"{self.instance.screening_identifier}</A>"
-            )
-            raise forms.ValidationError(msg)
+            if not subject_screening.eligible:
+                url_name = url_names.get("screening_listboard_url")
+                url = reverse(
+                    url_name,
+                    kwargs={"screening_identifier": self.instance.screening_identifier},
+                )
+                msg = mark_safe(
+                    "Not allowed. Subject is not eligible. "
+                    f'See subject <A href="{url}?q={screening_identifier}">'
+                    f"{screening_identifier}</A>"
+                )
+                raise forms.ValidationError(msg)
         return cleaned_data
 
 
-class SubjectRefusalForm(ConsentedFormMixin, FormValidatorMixin, forms.ModelForm):
+class SubjectRefusalForm(
+    AlreadyConsentedFormMixin, ScreeningFormMixin, FormValidatorMixin, forms.ModelForm
+):
 
     form_validator_cls = SubjectRefusalFormValidator
 
